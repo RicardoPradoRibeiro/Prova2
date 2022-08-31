3682578a0152db17c035c7f37771630124b7c85a
Ricardo Prado Ribeiro <ricardodopradoribeiro@hotmail.com>
2022-08-31 11:35:40 -0300

    Adicionar arquivos de projeto.

---
+++ Prova2/CadastroAluno.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova2
+{
+    public class CadastroAluno
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public string Escola { get; set; }
+
+        public double Media { get; set; }
+
+        public CadastroAluno()
+        {
+        }
+
+        public CadastroAluno(string nome, int idade, string escola)
+        {
+            Nome = nome;
+            Idade = idade;
+            Escola = escola;
+        }
+
+        public double Medias(double media)
+        {   
+            Media += media;
+            return Media;
+
+
+        }
+        
+        
+        public double NotaFinal()
+        {
+            return Media / 3.0;
+
+
+
+        }
+
+        public override string ToString()
+        {
+            return "Media final: " + NotaFinal().ToString("F1", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Prova2/Program.cs
@@ -0,0 +1,80 @@
+﻿using Prova2;
+using System;
+namespace ProgramaPrincipal
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe a Idade: ");
+            int.TryParse(Console.ReadLine(), out int idade);
+            Console.WriteLine("Informe a escola: ");
+            string escola = Console.ReadLine();
+            var aluno = new CadastroAluno(nome, idade, escola);
+            Console.WriteLine("--------------------");
+            
+
+
+
+
+            double media = 0.0;
+            for(int i = 1; i<= 3; i++)
+            {
+                Console.WriteLine($"Informe as notas do {i}º Bimestre:  ");
+                Console.WriteLine("Informe a nota do aluno em Matemática: ");
+                double.TryParse(Console.ReadLine(), out double notaMat);
+
+                Console.WriteLine("Informe a nota do aluno em Português: ");
+                double.TryParse(Console.ReadLine(), out double notaPort);
+
+                Console.WriteLine("Informe a nota do aluno em Ciências: ");
+                double.TryParse(Console.ReadLine(), out double notaCiencias);
+
+                Console.WriteLine("Informe a nota do aluno em Geografia: ");
+                double.TryParse(Console.ReadLine(), out double notaGeo);
+
+                Console.WriteLine("Informe a nota do aluno em História: ");
+                double.TryParse(Console.ReadLine(), out double notaHist);
+
+                Console.WriteLine("Informe a nota do aluno em Educação Física: ");
+                double.TryParse(Console.ReadLine(), out double notaEdFisica);
+
+                Console.WriteLine("Informe a nota do aluno em Ensino Religioso: ");
+                double.TryParse(Console.ReadLine(), out double notaEnsReligioso);
+
+                Console.WriteLine("Informe a nota do aluno em Inglês: ");
+                double.TryParse(Console.ReadLine(), out double notaIngles);
+
+                Console.WriteLine("Informe a nota do aluno em Artes: ");
+                double.TryParse(Console.ReadLine(), out double notaArtes);
+                media = ((notaMat + notaPort + notaCiencias + notaGeo + notaHist + notaEdFisica + notaEnsReligioso + notaIngles + notaArtes) / 9.0) / 10.0;
+                aluno.Medias(media);
+                
+            }
+
+
+            Console.WriteLine($"{aluno.Nome}, {aluno.Idade} anos, estuda na escola {aluno.Escola}.");
+            Console.WriteLine(aluno);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
